fca1d54da0d09f37d373cb21ddc5800802f3cadc
Alonzo Coeus <Alonzo.Coeus@mail.com>
2021-01-30 17:16:40 +0000

    Yet another version update

--- nixpkgs/xmonad.nix
+++ nixpkgs/xmonad.nix
@@ -8,8 +8,8 @@ let drv = { mkDerivation, base, containers, deepseq, hpack, lens, mtl
       src = nixpkgs.pkgs.fetchgit {
         url = "https://github.com/Alonzo-Coeus/.xmonad";
         name = "my-xmonad";
-        rev = "65867cc6af66f756c72a79a7f1f30ca64b541355";
-        sha256 = "a6bbc1731f430c214e700ac459d4f10484e799cffe46f16757e3234e3adf741f";
+        rev = "44717a2cb8ad2494715c299750ae501cae3df1a7";
+        sha256 = "1d3e07ae4f75967d2c5f4804a9dfaf7ba77ea9496ba27058e3f9fa2b74023b75";
       };
       isLibrary = false;
       isExecutable = true;
